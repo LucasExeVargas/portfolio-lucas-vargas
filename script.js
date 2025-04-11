f45fd1c4663bf92fd49d042ea5f8a8c6c83790d4
LucasExeVargas <lucasvargasexequiel@gmail.com>
2025-04-11 01:53:31 -0300

    cambio de colores

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     const checkbox = document.getElementById('checkbox');
     const body = document.body;
+    const textElements = document.querySelectorAll('.text-nav');
 
     // Activar tema claro al inicio
     body.classList.add('light-theme');
     checkbox.checked = true;
 
+    // Aplicar clases de texto según el tema inicial
+    textElements.forEach(el => {
+        el.classList.add('text-nav-light');
+        el.classList.remove('text-nav-dark');
+    });
+
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
             body.classList.remove('dark-theme');
             body.classList.add('light-theme');
-            
+
+            textElements.forEach(el => {
+                el.classList.remove('text-nav-dark');
+                el.classList.add('text-nav-light');
+            });
         } else {
-            
             body.classList.remove('light-theme');
             body.classList.add('dark-theme');
+
+            textElements.forEach(el => {
+                el.classList.remove('text-nav-light');
+                el.classList.add('text-nav-dark');
+            });
         }
     });
 });
