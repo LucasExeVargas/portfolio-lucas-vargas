056894230538b5223a449e229b8ce5871d170de6
LucasExeVargas <lucasvargasexequiel@gmail.com>
2025-04-19 19:37:27 -0300

    ...

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const checkbox = document.getElementById('checkbox');
     const body = document.body;
     const textElements = document.querySelectorAll('.text-nav');
-    const img = document.querySelector('.img_yo'); // <- Corrección aquí
+    const img = document.querySelector('.img-yo'); // <- Corrección aquí
 
     // Activar tema claro al inicio
     body.classList.add('light-theme');
     checkbox.checked = true;
-    img.src = 'img/yodia.svg'; // Imagen para tema claro
+    img.src = 'img/yo-dia.svg'; // Imagen para tema claro
 
     // Aplicar clases de texto según el tema inicial
     textElements.forEach(el => {
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             body.classList.remove('dark-theme');
             body.classList.add('light-theme');
 
-            img.src = 'img/yodia.svg'; // Imagen para tema claro
+            img.src = 'img/yo-dia.svg'; // Imagen para tema claro
 
             textElements.forEach(el => {
                 el.classList.remove('text-nav-dark');
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
             body.classList.remove('light-theme');
             body.classList.add('dark-theme');
 
-            img.src = 'img/yonoche.svg'; // Imagen para tema oscuro
+            img.src = 'img/yo-noche.svg'; // Imagen para tema oscuro
 
             textElements.forEach(el => {
                 el.classList.remove('text-nav-light');
