cbec3a189c5bf4947573459bdaee0ff40d1f4d60
LucasExeVargas <lucasvargasexequiel@gmail.com>
2025-04-21 16:25:56 -0300

    add new content

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const img = document.querySelector('.img-yo');
     const carousel = document.getElementById('carouselExampleCaptions');
     const btnMy = document.querySelectorAll('.custom-animated-btn');
+    const btnCV = document.querySelector('.btn-cv'); // Botón para descargar CV
+    const cards = document.querySelectorAll('.card');
+    const heads = document.querySelectorAll('.head');
+    const contents = document.querySelectorAll('.content');
 
     // Activar tema claro al inicio
     body.classList.add('light-theme');
@@ -18,6 +22,19 @@ document.addEventListener('DOMContentLoaded', () => {
         btn.classList.add('custom-animated-btn-light');
         btn.classList.remove('custom-animated-btn-dark');
     });
+    cards.forEach(card => {
+        card.classList.add('card-light');
+        card.classList.remove('card-dark');
+    });
+    heads.forEach(head => {
+        head.classList.add('head-light');
+        head.classList.remove('head-dark');
+    });
+    contents.forEach(content => {
+        content.classList.add('content-light');
+        content.classList.remove('content-dark');
+    });
+    
 
     checkboxes.forEach(checkbox => {
         checkbox.checked = true;
@@ -40,6 +57,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     btn.classList.add('custom-animated-btn-light');
                     btn.classList.remove('custom-animated-btn-dark');
                 });
+                cards.forEach(card => {
+                    card.classList.add('card-light');
+                    card.classList.remove('card-dark');
+                });
+                heads.forEach(head => {
+                    head.classList.add('head-light');
+                    head.classList.remove('head-dark');
+                });
+                contents.forEach(content => {
+                    content.classList.add('content-light');
+                    content.classList.remove('content-dark');
+                });
             } else {
                 body.classList.remove('light-theme');
                 body.classList.add('dark-theme');
@@ -53,7 +82,32 @@ document.addEventListener('DOMContentLoaded', () => {
                     btn.classList.add('custom-animated-btn-dark');
                     btn.classList.remove('custom-animated-btn-light');
                 });
+                cards.forEach(card => {
+                    card.classList.remove('card-light');
+                    card.classList.add('card-dark');
+                });
+                heads.forEach(head => {
+                    head.classList.remove('head-light');
+                    head.classList.add('head-dark');
+                });
+                contents.forEach(content => {
+                    content.classList.remove('content-light');
+                    content.classList.add('content-dark');
+                });
+                
             }
         });
     });
+
+    // Descargar PDF al hacer click en el botón
+    if (btnCV) {
+        btnCV.addEventListener('click', () => {
+            const link = document.createElement('a');
+            link.href = 'download\\CV-LucasVargas.pdf'; // Reemplaza con la ruta real de tu PDF
+            link.download = 'Lucas_Vargas_CV.pdf';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+    }
 });
