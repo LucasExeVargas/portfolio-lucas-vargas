76c6846e85384e6b870cf7720d835b58b4f85f63
LucasExeVargas <lucasvargasexequiel@gmail.com>
2025-04-21 02:50:09 -0300

    complete info of my

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const checkboxes = document.querySelectorAll('#checkbox'); // Ahora selecciona ambos
+    const checkboxes = document.querySelectorAll('#checkbox');
     const body = document.body;
     const textElements = document.querySelectorAll('.text-nav');
     const img = document.querySelector('.img-yo');
+    const carousel = document.getElementById('carouselExampleCaptions');
+    const btnMy = document.querySelectorAll('.custom-animated-btn');
 
     // Activar tema claro al inicio
     body.classList.add('light-theme');
     img.src = 'img/yo-dia.svg';
+    carousel.className = 'carousel carousel-dark slide';
     textElements.forEach(el => {
         el.classList.add('text-nav-light');
         el.classList.remove('text-nav-dark');
+    });
+    btnMy.forEach(btn => {
+        btn.classList.add('custom-animated-btn-light');
+        btn.classList.remove('custom-animated-btn-dark');
     });
 
     checkboxes.forEach(checkbox => {
@@ -24,17 +31,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 body.classList.remove('dark-theme');
                 body.classList.add('light-theme');
                 img.src = 'img/yo-dia.svg';
+                carousel.className = 'carousel carousel-dark slide';
                 textElements.forEach(el => {
                     el.classList.remove('text-nav-dark');
                     el.classList.add('text-nav-light');
+                });
+                btnMy.forEach(btn => {
+                    btn.classList.add('custom-animated-btn-light');
+                    btn.classList.remove('custom-animated-btn-dark');
                 });
             } else {
                 body.classList.remove('light-theme');
                 body.classList.add('dark-theme');
                 img.src = 'img/yo-noche.svg';
+                carousel.className = 'carousel slide';
                 textElements.forEach(el => {
                     el.classList.remove('text-nav-light');
                     el.classList.add('text-nav-dark');
+                });
+                btnMy.forEach(btn => {
+                    btn.classList.add('custom-animated-btn-dark');
+                    btn.classList.remove('custom-animated-btn-light');
                 });
             }
         });
