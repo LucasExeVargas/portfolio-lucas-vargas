539f6e47c358622b4c7af6a1996f943f17138849
LucasExeVargas <lucasvargasexequiel@gmail.com>
2025-04-22 21:58:39 -0300

    ...

--- conexion.php
+++ conexion.php
@@ -3,10 +3,9 @@ $localhost = "localhost";
 $username = "root";
 $password = "";
 $dbname = "proyecto-web";
-$con = new mysqli($localhost, $username, $password,$dbname);
-if($con->connect_error) {
- die("connection failed : " .$con->connect_error);
-}else{
+$con = new mysqli($localhost, $username, $password, $dbname);
+if ($con->connect_error) {
+    die("connection failed : " . $con->connect_error);
+} else {
     echo "coneccion exitosa";
 }
-?>
